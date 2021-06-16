8a5dd8c8327e60842a677947111c5d2ee356d277
Akbar Ahmadi <akbarsafari00@gmail.com>
2021-06-16 11:53:28 +0430

    first commit

---
+++ pages/auth/login.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Layout from "../components/layout";
+
+export default function Home() {
+
+
+  return (
+      <Layout>
+        <h1>
+          خوش امدید
+        </h1>
+      </Layout>
+  )
+}
